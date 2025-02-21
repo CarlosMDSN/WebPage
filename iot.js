1585e842bab605438008ebbd11e1bad09384bbde
CarlosM <G00429041@atu.ie>
2025-02-21 17:47:51 +0000

    Navigation Bar, Product Details + CSS added

--- iot.js
+++ iot.js
@@ -1,10 +1,8 @@
-/* Open */
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  }
-  
-  /* Close */
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  }
-  
+
+
+
+
+
+
+
+
